f81fc49b654890d57e1247932ee67792bda774cf
Aqiff Mursyideen <47936968+aqiff12@users.noreply.github.com>
2020-12-29 19:52:20 +0800

    Add Lukman Assignment 3

---
+++ Assignment/LukmanHakim_17170546/Assignment3/WID170026_AI_Gaming Assignment 3/Assets/MazeGenerator.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour
+{
+
+    List<List<Vector3>> Grid;
+    List<List<Cell>> CellGrid;
+    ObjectPoints PointsOnThePlane;
+    int MazeWidth = 11;
+    int MazeHeight = 11;
+    readonly int planeHeight = 11;
+    readonly int planeWidth = 11;
+    GameObject BorderParent;//Empty parent object to keep borders transform
+    RecursiveBacktracker RB;
+    UIInfoManager UIIM;
+
+    void Start()
+    {
+        UIIM = GetComponent<UIInfoManager>();
+        BorderParent = new GameObject("BorderParent"); 
+        PointsOnThePlane = this.GetComponent<ObjectPoints>();
+        RB = new RecursiveBacktracker();
+        CreateNewMaze();       
+    }
+    
+    public void CreateNewMaze()
+    {
+        Destroy(BorderParent);
+        BorderParent = new GameObject("BorderParent");
+
+        MazeWidth = Random.Range(2,planeWidth+1);
+        MazeHeight = Random.Range(2, planeHeight+1);
+        UIIM.UpdateUI(MazeHeight,MazeWidth);
+        CreateTheGrid();
+        CellGrid = RB.GetNewMaze(Grid);
+
+        ShowMaze();
+    }
+
+    void CreateTheGrid()  //creates grid with given width and length from the points on the plane
+    {
+        Grid = new List<List<Vector3>>();
+        List<Vector3> OneRow = new List<Vector3>();
+
+        for (int y = 0; y < MazeHeight; y++)
+        {
+            for (int x = 0; x < MazeWidth; x++)
+            {
+                OneRow.Add(PointsOnThePlane.GetObjectGlobalVertices()[y + x * planeHeight]); //getting the points on the plane from the upper left corner
+            }
+            Grid.Add(OneRow);
+            OneRow = new List<Vector3>();
+        }
+    }
+
+    void ShowMaze()  //Creates maz with borders as cubes
+    {
+        GameObject VerticalBorder = GameObject.CreatePrimitive(PrimitiveType.Cube);  //vertically scaled cube for up and down borders
+        VerticalBorder.transform.localScale = new Vector3(1.5f, 1, 0.5f);
+
+        GameObject HorizontalBorder = GameObject.CreatePrimitive(PrimitiveType.Cube);//horizontally scaled cube for left and right borders
+        HorizontalBorder.transform.localScale = new Vector3(0.5f, 1, 1.5f);
+
+        for (int x = 0; x < MazeWidth; x++)
+        {
+            for (int y = 0; y < MazeHeight; y++)
+            {
+                if (CellGrid[y][x].Borders.Contains(Direction.Up))
+                {
+                    PlaceHorizontalBorder(HorizontalBorder, CellGrid[y][x], Direction.Up);
+                }
+                if (CellGrid[y][x].Borders.Contains(Direction.Down))
+                {
+                    PlaceHorizontalBorder(HorizontalBorder, CellGrid[y][x], Direction.Down);
+                }
+
+                if (CellGrid[y][x].Borders.Contains(Direction.Right))
+                {
+                    PlaceVerticalBorder(VerticalBorder, CellGrid[y][x], Direction.Right);
+                }
+                if (CellGrid[y][x].Borders.Contains(Direction.Left))
+                {
+                    PlaceVerticalBorder(VerticalBorder, CellGrid[y][x], Direction.Left);
+                }
+            }
+        }
+        GameObject.Destroy(VerticalBorder); //destroy source objects
+        GameObject.Destroy(HorizontalBorder);
+    }
+
+    void PlaceHorizontalBorder(GameObject border, Cell c, Direction d) //borders are put moved away from the point to suround it
+    {                                                   //upper border moved up, and lower border moved down along x axis
+        Instantiate(border, c.position + Vector3.right * 0.5f * ((d == Direction.Up) ? 1 : -1), Quaternion.identity, BorderParent.transform);
+    }
+
+    void PlaceVerticalBorder(GameObject border, Cell c, Direction d)
+    {                                                   //left border moved left, and right border moved right along z axis
+        Instantiate(border, c.position + Vector3.forward * 0.5f * ((d == Direction.Left) ? 1 : -1), Quaternion.identity, BorderParent.transform);
+    }
+
+
+
+
+}
---
+++ Assignment/LukmanHakim_17170546/Assignment3/WID170026_AI_Gaming Assignment 3/Assets/RecursiveBacktracker.cs
@@ -0,0 +1,216 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class Cell {  //a cell consists of its position and borders
+    public List<Direction> Borders;
+    public Vector3 position;
+  public  Cell(Vector3 pos)
+    {
+        position = pos;
+        Borders = new List<Direction>();
+    }
+}
+
+   public enum Direction //direction's bitwise value
+{
+        Up = 1, //0001
+        Down = 2, //0010
+        Right = 4, //0100
+        Left= 8   //1000
+    }
+
+public class RecursiveBacktracker 
+{
+
+    enum DirectionX // direction's x axis movement value
+    {
+        Right = 1,
+        Left = -1,
+        Up = 0,
+        Down = 0
+    }
+    enum DirectionY //returns direction's y axis movement value
+    {
+        Right = 0,
+        Left = 0,
+        Up = -1,
+        Down = 1
+    }
+    enum Opposite//opposite of direction's bitwise value
+    {
+        Right = 8,
+        Left = 4,
+        Up = 2,
+        Down = 1
+    }
+
+    List<List<int>> Grid;
+   List<List<Cell>> CellGrid;
+
+   int GridHeight = 11;
+   int GridWidth = 11;
+
+
+    public List<List<Cell>> GetNewMaze(List<List<Vector3>> Maze)
+    {
+        SetGridBounds(Maze);
+        CreateGrid(Maze);
+        CarvePassagesFrom(0, 0, Grid);
+        FillMazeValues();
+        return CellGrid;
+    }
+  
+
+    void SetGridBounds(List<List<Vector3>> Maze)
+    {
+        GridWidth = Maze[0].Count();
+        GridHeight = Maze.Count();
+    }
+
+    void CreateGrid(List<List<Vector3>> Maze)  //CellGrid and Grid variables are initially created
+    {
+        Grid = new List<List<int>>();
+        CellGrid = new List<List<Cell>>();
+        List<Cell> cellRow = new List<Cell>();
+        List<int> OneRow = new List<int>();
+        for (int a = 0; a < GridHeight; a++)
+        {
+            for (int b = 0; b < GridWidth; b++)
+            {
+                cellRow.Add(new Cell(Maze[a][b]));
+                OneRow.Add(0);
+            }
+            CellGrid.Add(cellRow);
+            Grid.Add(OneRow);
+            cellRow = new List<Cell>();
+            OneRow = new List<int>();
+        }
+    }
+
+    void CarvePassagesFrom(int cx, int cy, List<List<int>> Grid) //Implementation of recursive backtracking algorithm
+    {
+        List<Direction> Directions = new List<Direction>{Direction.Up,Direction.Down,Direction.Right,Direction.Left};
+
+        Directions = Randomize(Directions);
+
+        foreach (Direction dir in Directions) //check each direction
+        {
+            int nx = cx +   DirToDirectionX(dir);//translate current coordinate to drections coordinate
+            int ny = cy + DirToDirectionY(dir);
+
+            if ( (0 <= ny && ny<= GridHeight - 1) &&  (0 <= nx && nx <= GridWidth - 1 )) //if new coordinate between the bounds
+            {
+                if ( Grid[ny][nx] == 0) { //If the new coordinate unvisited
+                Grid[cy][cx] |= (int)dir; //current direction added to current cell bitwise
+                Grid[ny][nx] |= DirToOpposite(dir);//opposite of current direction added to next cell bitwise
+                CarvePassagesFrom(nx, ny, Grid);//Carving the map continued from the next cell 
+            }
+            }
+        }
+    }
+
+    void FillMazeValues()
+    {
+        string AsciiMapRepresentation = ""; //These are used to show the map as ascii representation and bit values of cells
+        string BitwiseMapRepresentation = "";
+
+        for (int t = 0; t < GridHeight; t++) //leftest border values 
+        {
+            CellGrid[t][0].Borders.Add(Direction.Left);
+
+        }
+        for (int t = 0; t < GridWidth; t++)//uppest border values 
+        {
+            CellGrid[0][t].Borders.Add(Direction.Up);
+
+        }
+        for (int y = 0; y < GridHeight; y++)
+        {
+             
+             AsciiMapRepresentation += "\n|";
+             BitwiseMapRepresentation += "\n";
+
+            for (int x = 0; x < GridWidth; x++)
+            {
+
+                BitwiseMapRepresentation += "(" + Grid[y][x] + ")";
+                AsciiMapRepresentation += (((Grid[y][x] & (int)Direction.Down) != 0)? " ":"_") ;
+                
+                if ((Grid[y][x] & (int)Direction.Down) == 0) //there is a wall in the downside of the map
+                {
+                    CellGrid[y][x].Borders.Add(Direction.Down);
+                }
+                if ((Grid[y][x] & (int)Direction.Right) == 0)//There is a wall in the rightside of the cell
+                {                
+                    CellGrid[y][x].Borders.Add(Direction.Right);
+                    AsciiMapRepresentation += "|"  ;
+                }
+            }           
+        }            
+ 
+            Debug.Log(AsciiMapRepresentation);  //Mapps are printed to console
+            Debug.Log(BitwiseMapRepresentation);     
+    }
+    int DirToDirectionX(Direction dir)//returns direction's x axis movement value
+    {
+
+        switch (dir)
+        {
+            case Direction.Up:
+                return (int)DirectionX.Up;
+            case Direction.Down:
+                return (int)DirectionX.Down;
+            case Direction.Left:
+                return (int)DirectionX.Left;
+            case Direction.Right:
+                return (int)DirectionX.Right;
+        }
+        return 0;
+    }
+    int DirToDirectionY(Direction dir) //returns direction's y axis movement value
+    {
+        switch (dir)
+        {
+            case Direction.Up:
+                return (int)DirectionY.Up;
+            case Direction.Down:
+                return (int)DirectionY.Down;
+            case Direction.Left:
+                return (int)DirectionY.Left;
+            case Direction.Right:
+                return (int)DirectionY.Right;
+        }
+        return 0;
+    }
+    int DirToOpposite(Direction dir) //returns direction's opposite direction value
+    {
+        switch (dir)
+        {
+            case Direction.Up:
+                return (int)Opposite.Up;
+            case Direction.Down:
+                return (int)Opposite.Down;
+            case Direction.Left:
+                return (int)Opposite.Left;
+            case Direction.Right:
+                return (int)Opposite.Right;
+        }
+        return 0;
+    }
+
+    List<Direction> Randomize(List<Direction> Directions)
+    {
+        for (int i = 0; i < Directions.Count; i++)
+        {
+            Direction temp = Directions[i];
+            int randomIdx = UnityEngine.Random.Range(i, Directions.Count);
+            Directions[i] = Directions[randomIdx];
+            Directions[randomIdx] = temp;
+        }
+        return Directions;
+    }
+
+}
